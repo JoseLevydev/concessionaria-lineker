53acaefe8c89c7c8c145923aaab66a08d5d0dd0a
José Levy Fernandes Lacerda <134818545+JoseLevydev@users.noreply.github.com>
2024-05-18 17:58:07 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,29 +4,31 @@ export default function LoginPage() {
       <main className='flex flex-col h-screen w-screen items-center justify-center bg-carro-background bg-cover'>
         <div className='mx-auto flex w-full max-w-sm flex-col gap-6'>
           <div className='flex flex-col items-center'>
-            <h1 className='text-4xl font-semibold text-zinc-900'>Sign In</h1>
-            <p className='text-sm text-stone-900'>Sign in to access your account</p>
+            <h1 className='text-4xl font-semibold text-white'>ARCHIP</h1>
+            <p className='text-sm text-gray-50'>Entre para acessar sua conta</p>
           </div>
           <div className='form-group'>
             <div className='form-field'>
-              <label className='form-label'>Email address</label>
+              <label className='form-label text-gray-50 font-semibold'>
+                Endereço de email
+              </label>
 
               <input
-                placeholder='Type here'
+                placeholder='Escreva aqui'
                 type='email'
                 className='input max-w-full'
               />
               <label className='form-label'>
-                <span className='form-label-alt'>
-                  Please enter a valid email.
-                </span>
+                <span className='form-label-alt'>Digite um email valido.</span>
               </label>
             </div>
             <div className='form-field'>
-              <label className='form-label'>Password</label>
+              <label className='form-label text-gray-50 font-semibold'>
+                Senha
+              </label>
               <div className='form-control'>
                 <input
-                  placeholder='Type here'
+                  placeholder='Escreva aqui'
                   type='password'
                   className='input max-w-full'
                 />
@@ -36,19 +38,22 @@ export default function LoginPage() {
               <div className='form-control justify-between'>
                 <div className='flex gap-2'>
                   <input type='checkbox' className='checkbox' />
-                  <a href='#'>Remember me</a>
+                  <a href='#'>Lembre de mim</a>
                 </div>
                 <label className='form-label'>
                   <a className='link link-underline-hover link-primary text-sm'>
-                    Forgot your password?
+                    Esqueceu sua senha?
                   </a>
                 </label>
               </div>
             </div>
             <div className='form-field pt-5'>
               <div className='form-control justify-between'>
-                <button type='button' className='btn bg-white text-zinc-900 w-full'>
-                  Sign in
+                <button
+                  type='button'
+                  className='btn bg-white text-zinc-900 w-full'
+                >
+                  Log in
                 </button>
               </div>
             </div>
@@ -56,7 +61,7 @@ export default function LoginPage() {
             <div className='form-field'>
               <div className='form-control justify-center'>
                 <a className='link link-underline-hover link-primary text-sm'>
-                  Don't have an account yet? Sign up.
+                  Não tem uma conta? Registre-se.
                 </a>
               </div>
             </div>
@@ -65,5 +70,4 @@ export default function LoginPage() {
       </main>
     </>
   );
-
 }
